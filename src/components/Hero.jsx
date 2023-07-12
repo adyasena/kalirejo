0477856db6054c5de6c8fbf5fb2876c0f85ba909
adyasena <adyasena01@mail.ugm.ac.id>
2023-07-12 18:38:14 +0700

    update navbar and hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { BgHome, Bg, Kering, Logo, Pattern } from "../assets";
+import { Bg, Logo } from "../assets";
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
 const Hero = () => {
@@ -18,16 +18,18 @@ const Hero = () => {
   return (
     <>
       <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${Bg})`}}>
-        <div className="w-full h-full bg-gradient-to-t from-white bg-opacity-100">
-          <div className="flex flex-col h-full container mx-auto items-center font-poppins text-green">
+        <div className="w-full h-full bg-gradient-to-t from-white">
+          <div className="flex flex-col h-full container mx-auto items-center font-poppins text-green-dark">
             <div className="flex flex-col justify-center items-center mt-36 font-semibold lg:text-4xl text-2xl text-center leading-normal gap-8">
               <img src={Logo} className="lg:w-1/12 w-1/4"/>
-              Desa Kalirejo
-              <div className="lg:text-xl text-lg font-normal">
-                Temukan produk UMKM unggulan<br/>
-                dan destinasi wisata terbaik!
-              </div>
-              <button className="bg-green text-white mt-12 font-normal text-xl py-2 px-6 rounded-md transform duration-300 ease hover:bg-green-dark"
+              <p className="lg:text-4xl text-lg font-bold">
+                Temukan destinasi wisata terbaik<br/>
+                dan produk UMKM unggulan
+              </p>
+              <p className="lg:text-xl font-normal">
+                Desa Kalirejo<br/>Salaman, Magelang, Jawa Tengah
+              </p>
+              <button className="bg-green-dark text-white mt-12 font-normal text-xl py-2 px-6 rounded-md transform duration-300 ease lg:hover:bg-black"
                 onClick={handleClickScroll}>
                 Telusuri Desa
               </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,19 +19,56 @@ const Navbar = () => {
   const toHome = () => {
     navigate('/');
   };
+  const toWisata = () => {
+    navigate('/');
+  };
+  const toUMKM = () => {
+    navigate('/');
+  };
+  const toGaleri = () => {
+    navigate('/');
+  };
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <div className="navbar">
       <div className={"navbar bg-white "
-        + (scroll ? "opacity-100 shadow-md" : "h-24 opacity-0")}>
+        + (scroll ? "opacity-100 shadow-md" : "lg:h-24 opacity-0")}>
       </div>
-      <div className={"z-[1] font-poppins sticky flex flex-row container mx-auto lg:px-8 text-center items-center text-lg font-semibold text-green " +
-        "justify-between transform duration-300 ease overflow-hidden " + (scroll ? "h-16" : "h-24")}>
+      <div className={"z-[1] font-poppins sticky flex flex-row container mx-auto lg:px-8 text-center items-center text-lg font-semibold text-green-dark " +
+        "justify-between transform duration-300 ease overflow-hidden " + (scroll ? "h-16" : "lg:h-24 h-16")}>
         <button className="flex flex-row justify-start items-center h-full"
           onClick={toHome}>
           <img src={Logo} className={"m-2 transform duration-300 ease " + (scroll ? "w-8" : "w-0 mr-[-4px]")}/>
           <p className="transform">Kalirejo Lestari</p> 
         </button>
+        <div className="flex flex-row gap-12">
+          <button className="hover:underline"
+            onClick={toHome}>
+            Beranda
+          </button>
+          <button className="hover:underline"
+            onClick={toWisata}>
+            Wisata
+          </button>
+          <button className="hover:underline"
+            onClick={toUMKM}>
+            UMKM
+          </button>
+          <button className="hover:underline"
+            onClick={toGaleri}>
+            Galeri
+          </button>
+          <button className="hover:underline"
+            onClick={scrollToBottom}>
+            Kontak
+          </button>
+        </div>
       </div>
     </div>
   );
