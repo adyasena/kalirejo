0becee785558b057b9726cb8dd51e3b5a3876bc3
adyasena <adyasena01@mail.ugm.ac.id>
2023-07-19 01:33:52 +0700

    add image carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,8 @@ const Hero = () => {
       <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${Bg})`}}>
         <div className="w-full h-full bg-gradient-to-t from-white">
           <div className="flex flex-col h-full container mx-auto items-center font-poppins text-green-dark">
-            <div className="flex flex-col h-full items-center justify-between lg:justify-center mt-36 lg:mt-0 font-semibold lg:text-center leading-normal gap-8">
+            <div className="flex flex-col h-full items-center justify-between lg:justify-center pt-36 lg:pt-0 font-semibold lg:text-center leading-normal gap-8">
+              <div className="hidden lg:flex h-20"></div>
               <img src={Logo} className="lg:w-1/12 w-1/3 lg:my-0 my-auto"/>
               <div className="flex flex-col gap-8 lg:items-center">
                 <p className="lg:text-4xl text-5xl font-bold">
@@ -29,15 +30,18 @@ const Hero = () => {
                 <p className="lg:text-xl text-lg font-normal lg:w-4/5">
                   Temukan destinasi wisata terbaik dan produk UMKM unggulan di Desa Kalirejo, Kecamatan Salaman, Kabupaten Magelang, Jawa Tengah
                 </p>
-                <button className="bg-green-dark text-white mb-32 lg:mb-0 font-normal lg:text-xl text-lg py-2 px-6 rounded-lg transform duration-300 ease lg:hover:bg-black"
+                <button className="bg-green-dark text-white font-normal lg:text-xl text-lg py-2 px-6 rounded-lg transform duration-300 ease lg:hover:bg-black"
                   onClick={handleClickScroll}>
                     Telusuri Sekarang
                 </button>
-              </div>              
+                <div className="lg:h-2"></div>
+                
+              </div>
+              <div id="section-1" className="pb-10 lg:pb-0"></div>
             </div>
+            
           </div>
         </div>
-        <div id="section-1"></div>
       </div>
     </>
   );
