92fb29af192c069ae75fd69dd5b7a7d6eddc67df
adyasena <adyasena01@mail.ugm.ac.id>
2023-08-01 00:53:39 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ mongoose
 mongoose.connection.once('open', () => {
   app.use(fileupload({useTempFiles: true}))
   app.use(cors({
+    origin: 'http://localhost:5173',
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   }));
