3e9c161e9871c555ba4d0f34991d5ebadfc420ce
adyasena <adyasena01@mail.ugm.ac.id>
2023-07-31 20:02:21 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ mongoose.connection.once('open', () => {
   app.use(fileupload({useTempFiles: true}))
   app.use(cors());
   app.use(express.json())
-  app.use(express.urlencoded({ extended: false }))
+  app.use(express.urlencoded({ extended: true }))
   app.use(express.static('public'));
 
   app.use("/umkm", require("./routes/umkmRoutes"))
